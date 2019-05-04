df7d79bf0aee8eea6b6ccde5c181579192eb5b2e
pimrampha <pimrampha.m@gmail.com>
2019-05-04 09:59:24 +0700

    Update indent in date.test.js

--- src/lib/date.test.js
+++ src/lib/date.test.js
@@ -1,25 +1,19 @@
 import { getTotalDateByYear, getTotalDateByMonth, isLeapYear } from './date.js';
 
-
 describe('isLeapYear', () => {
-    test('year 2000 is leap year ', () => {
-        expect(isLeapYear(2000)).toBe(true);
-    });
+  test('year 2000 is leap year ', () => {
+    expect(isLeapYear(2000)).toBe(true);
+  });
 
-    test('year 1990 isn\'t leap year ', () => {
-        expect(isLeapYear(1990)).toBe(false);
-    });
+  test('year 1990 isn\'t leap year ', () => {
+    expect(isLeapYear(1990)).toBe(false);
+  });
 });
 
-test('getTotalDateByYear', () => {
-    expect(getTotalDateByYear(1900)).toBe(365);
+test('year 1900 getTotalDateByYear', () => {
+  expect(getTotalDateByYear(1900)).toBe(365);
 });
 
-test('getTotalDateByMonth', () => {
-    expect(getTotalDateByMonth(3, 2000)).toBe(91);
+test('march in year 2000 getTotalDateByMonth', () => {
+  expect(getTotalDateByMonth(3, 2000)).toBe(91);
 });
-
-
-
-
-
